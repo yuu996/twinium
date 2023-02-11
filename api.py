280b30d3258464895ae536be42feb7cce73e702d
Asus-DesktopPC-yuu <ydiangang82@gmail.com>
2023-02-11 13:13:50 +0900

    Update api.py add destroy_block,destroy_mute,is_follower

--- api.py
+++ api.py
@@ -8,6 +8,10 @@ import time
 #-----------------------------------------------------------------------------------------------------------------------
 # Latest change 2023/02/11
 # Add create_block,get_followers,create_mute
+
+# Latest change 2023/02/11 13:04
+# Add destroy_block,destroy_mute,is_follower
+
 #-----------------------------------------------------------------------------------------------------------------------
 
 
@@ -24,7 +28,7 @@ def update_status(tweet_content):
     update_status(tweet_content | Str)
     """
     driver.get("https://twitter.com/compose/tweet")
-    time.sleep(1)
+    driver.implicitly_wait(1)
     driver.find_element(by=By.CLASS_NAME, value="notranslate").send_keys(tweet_content)
     driver.find_element(by=By.XPATH, value="//div[@data-testid='tweetButton']").click()
 
@@ -61,6 +65,25 @@ def create_block(screen_name):
     driver.implicitly_wait(0.5)
     driver.find_element(By.XPATH, '//div[@data-testid="confirmationSheetConfirm"]').click()
 
+def destroy_block(screen_name):
+    """
+    destroy_block(screen_name | Str)
+    """
+    driver.get(f"https://twitter.com/{screen_name}")
+    driver.implicitly_wait(1)
+    try:
+        is_blocknow = driver.find_element(By.XPATH, value='//div[@data-testid="empty_state_body_text"]')
+        if is_blocknow:
+            driver.implicitly_wait(0.5)
+            driver.find_element(By.CLASS_NAME,
+                                value="css-18t94o4.css-1dbjc4n.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-2yi16.r-1qi8awa.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr").click()
+            driver.implicitly_wait(0.5)
+            driver.find_element(By.CLASS_NAME,
+                                value="css-18t94o4.css-1dbjc4n.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-16y2uox.r-6gpygo.r-peo1c.r-1ps3wis.r-1ny4l3l.r-1udh08x.r-1guathk.r-1udbk01.r-o7ynqc.r-6416eg.r-lrvibr.r-3s2u2q").click()
+    except selenium.common.exceptions.NoSuchElementException:
+        print("このユーザーをブロックしていませんでした")
+
+
 def create_mute(screen_name):
     """
     create_mute(screen_name | Str)
@@ -70,3 +93,26 @@ def create_mute(screen_name):
     driver.find_element(By.CLASS_NAME,value="css-18t94o4.css-1dbjc4n.r-1niwhzg.r-sdzlij.r-1phboty.r-rs99b7.r-6gpygo.r-1kb76zh.r-2yi16.r-1qi8awa.r-1ny4l3l.r-o7ynqc.r-6416eg.r-lrvibr").click()
     driver.implicitly_wait(0.5)
     driver.find_element(By.XPATH, value='//div[@data-testid="mute"]').click()
+
+def destroy_mute(screen_name):
+    """
+    destroy_mute(screen_name | Str)
+    """
+    driver.get(f"https://twitter.com/{screen_name}")
+    driver.implicitly_wait(1)
+    try:
+        driver.find_element(By.XPATH, value='//span[@data-testid="unmuteLink"]')
+        driver.find_element(By.XPATH, value='//div[@data-testid="userActions"]').click()
+        driver.find_element(By.XPATH, value='//div[@data-testid="mute"]').click()
+    except selenium.common.exceptions.NoSuchElementException:
+        print("ミュートしていませんでした")
+
+def is_follower(screen_name):
+    driver.get(f"https://twitter.com/{screen_name}")
+    driver.implicitly_wait(1)
+    try:
+        is_follower = driver.find_element(By.XPATH, value='//div[@data-testid="userFollowIndicator"]')
+        if is_follower:
+            return True
+    except selenium.common.exceptions.NoSuchElementException:
+        return False
