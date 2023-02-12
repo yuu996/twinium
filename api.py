8b390166184763867069f1e70b0d4e3d9bf83100
Asus-DesktopPC-yuu <ydiangang82@gmail.com>
2023-02-12 20:21:04 +0900

    Update api.py

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-import selenium
 from selenium.common.exceptions import NoSuchElementException
 
 #-----------------------------------------------------------------------------------------------------------------------
